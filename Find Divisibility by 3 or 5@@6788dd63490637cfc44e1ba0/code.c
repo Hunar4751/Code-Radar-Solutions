3c23a65f783329dee2a85c54f2b458dfeedc2dad
Hunar4751 <hunar1349.becse24@chitkara.edu.in>
2025-01-31 16:57:00 +0530

    Solution to Find Divisibility by 3 or 5

--- Find Divisibility by 3 or 5@@6788dd63490637cfc44e1ba0/code.c
+++ Find Divisibility by 3 or 5@@6788dd63490637cfc44e1ba0/code.c
@@ -7,8 +7,8 @@ int main() {
         printf("Divisible");
     }else if(a%3 == 0){
         printf("Divisible by 3");
-//     }else if (a%5 == 0){
-// printf("Divisible by 5");
+    }else if (a%5 == 0){
+printf("Divisible by 5");
 //     }else{
 //         printf("Not Divisible")
     }
